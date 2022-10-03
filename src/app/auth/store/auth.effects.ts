b924b13b3d78e6e23c5bc980361911f0c98b0cd8
Panchal, Mitul <Mitul.Panchal@dieboldnixdorf.com>
2022-10-03 18:52:17 +0530

    Add Auto Login and Auto Logout
    - Add Auto Login and Auto Logout functionality entirely
      driven through NgRx

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -7,6 +7,8 @@ import { Injectable } from '@angular/core';
 
 import * as AuthActions from './auth.actions';
 import { Router } from '@angular/router';
+import { User } from '../user.model';
+import { AuthService } from '../auth.service';
 
 export interface AuthResponseData {
   kind: string;
@@ -20,6 +22,8 @@ export interface AuthResponseData {
 
 const handleAuthentication = (expiresIn: number, email: string, userId: string, token: string) => {
   const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+  const user = new User(email, userId, token, expirationDate);
+  localStorage.setItem('userData', JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({email: email, userId: userId, token: token, expirationDate: expirationDate});
 }
 
@@ -57,7 +61,11 @@ export class AuthEffects {
           returnSecureToken: true
         }
       )
-      .pipe(map(resData => {
+        .pipe(
+          tap((resData) => {
+            this.authService.setLogoutTimer(+resData.expiresIn * 1000)
+        }),
+        map(resData => {
         return handleAuthentication(+resData.expiresIn, resData.email, resData.localId, resData.idToken)
       }), catchError(errorRes => {
         return handleError(errorRes)
@@ -75,7 +83,11 @@ export class AuthEffects {
             password: authData.payload.password,
             returnSecureToken: true
           }
-      ).pipe(map(resData => {
+      ).pipe(
+        tap((resData) => {
+          this.authService.setLogoutTimer(+resData.expiresIn * 1000)
+        }),
+        map(resData => {
         return handleAuthentication(+resData.expiresIn, resData.email, resData.localId, resData.idToken)
       }), catchError(errorRes => {
         return handleError(errorRes);
@@ -83,12 +95,52 @@ export class AuthEffects {
     }),
   );
 
+  @Effect()
+  autoLogin = this.actions$.pipe(ofType(AuthActions.AUTO_LOGIN),
+    map(() => {
+      const userData: {
+        email: string;
+        id: string;
+        _token: string;
+        _tokenExpirationDate: string;
+      } = JSON.parse(localStorage.getItem('userData'));
+      if (!userData) {
+        return {type: 'DUMMY'};
+      }
+
+      const loadedUser = new User(
+        userData.email,
+        userData.id,
+        userData._token,
+        new Date(userData._tokenExpirationDate)
+      );
+
+      if (loadedUser.token) {
+        //this.user.next(loadedUser);
+        const expirationDuration =
+          new Date(userData._tokenExpirationDate).getTime() -
+          new Date().getTime();
+        this.authService.setLogoutTimer(expirationDuration)
+        return new AuthActions.AuthenticateSuccess({ email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate) });
+       /*  const expirationDuration =
+          new Date(userData._tokenExpirationDate).getTime() -
+          new Date().getTime();
+        this.autoLogout(expirationDuration); */
+      }
+      return {type: 'DUMMY'}
+    })
+    )
+
   @Effect({ dispatch: false })
-  authRedirect = this.actions$.pipe(ofType(AuthActions.AUTHENTICATE_SUCCESS, AuthActions.LOGOUT), tap(() => {
+  authRedirect = this.actions$.pipe(ofType(AuthActions.AUTHENTICATE_SUCCESS), tap(() => {
     this.router.navigate(['/'])
   }));
 
-  constructor(private actions$: Actions, private http: HttpClient, private router: Router) {
-
-  }
+  @Effect({ dispatch: false })
+  authLogout = this.actions$.pipe(ofType(AuthActions.LOGOUT), tap(() => {
+    this.authService.clearLogoutTimer();
+    localStorage.removeItem('userData');
+    this.router.navigate(['/auth']);
+  }));
+  constructor(private actions$: Actions, private http: HttpClient, private router: Router, private authService: AuthService) {}
 }
