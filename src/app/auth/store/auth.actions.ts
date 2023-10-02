fe842fb74b833d7c1f53a98b158301bfe77e6f05
Mitul Panchal <mitul327@gmail.com>
2023-10-02 17:51:57 +0530

    Fetch Recipes using Resolver
    - Fetch Recipes using Resolve Guard
    - Fix Auth Redirect Functionality

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,17 +1,24 @@
 import { Action } from "@ngrx/store";
 
-
-export const LOGIN_START = '[Auth] Login Start';
-export const AUTHENTICATE_SUCCESS = '[Auth] Login';
-export const AUTHENTICATE_FAIL = '[Auth] Login Fail';
-export const SIGNUP_START = '[Auth] Signup Start';
-export const CLEAR_ERROR = '[Auth] Clear Error';
-export const LOGOUT = '[Auth] Logout';
-export const AUTO_LOGIN = '[Auth] Auto Login';
+export const LOGIN_START = "[Auth] Login Start";
+export const AUTHENTICATE_SUCCESS = "[Auth] Login";
+export const AUTHENTICATE_FAIL = "[Auth] Login Fail";
+export const SIGNUP_START = "[Auth] Signup Start";
+export const CLEAR_ERROR = "[Auth] Clear Error";
+export const LOGOUT = "[Auth] Logout";
+export const AUTO_LOGIN = "[Auth] Auto Login";
 
 export class AuthenticateSuccess implements Action {
   readonly type = AUTHENTICATE_SUCCESS;
-  constructor(public payload: { email: string, userId: string, token: string; expirationDate: Date }) { }
+  constructor(
+    public payload: {
+      email: string;
+      userId: string;
+      token: string;
+      expirationDate: Date;
+      redirect: boolean;
+    }
+  ) {}
 }
 
 export class Logout implements Action {
@@ -20,7 +27,7 @@ export class Logout implements Action {
 
 export class LoginStart implements Action {
   readonly type = LOGIN_START;
-  constructor(public payload: {email: string, password: string}) {}
+  constructor(public payload: { email: string; password: string }) {}
 }
 
 export class AuthenticateFail implements Action {
@@ -30,7 +37,7 @@ export class AuthenticateFail implements Action {
 
 export class SignupStart implements Action {
   readonly type = SIGNUP_START;
-  constructor(public payload: {email: string, password: string}) {}
+  constructor(public payload: { email: string; password: string }) {}
 }
 
 export class ClearError implements Action {
@@ -41,4 +48,11 @@ export class AutoLogin implements Action {
   readonly type = AUTO_LOGIN;
 }
 
-export type AuthActions = AuthenticateSuccess | Logout | LoginStart | AuthenticateFail | SignupStart | ClearError | AutoLogin;
+export type AuthActions =
+  | AuthenticateSuccess
+  | Logout
+  | LoginStart
+  | AuthenticateFail
+  | SignupStart
+  | ClearError
+  | AutoLogin;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
-import { DataStorageService } from '../shared/data-storage.service';
-import { AuthService } from '../auth/auth.service';
-import { Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Store } from "@ngrx/store";
+import { map } from "rxjs/operators";
 
-import * as fromApp from '../store/app.reducer';
-import * as AuthActions from '../auth/store/auth.actions';
+import * as fromApp from "../store/app.reducer";
+import * as AuthActions from "../auth/store/auth.actions";
+import * as RecipeActions from "../recipes/store/recipe.actions";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html",
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
@@ -21,14 +22,17 @@ export class HeaderComponent implements OnInit, OnDestroy {
     private dataStorageService: DataStorageService,
     private authService: AuthService,
     private store: Store<fromApp.AppState>
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.userSub = this.store.select('auth').pipe(map(authState => authState.user)).subscribe(user => {
-      this.isAuthenticated = !!user;
-      console.log(!user);
-      console.log(!!user);
-    });
+    this.userSub = this.store
+      .select("auth")
+      .pipe(map((authState) => authState.user))
+      .subscribe((user) => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+      });
   }
 
   onSaveData() {
@@ -36,12 +40,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    //this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 
   onLogout() {
     this.authService.logout();
-    this.store.dispatch(new AuthActions.Logout())
+    this.store.dispatch(new AuthActions.Logout());
   }
 
   ngOnDestroy() {
