a211803331f7a12d6540eb0ea046f8564ef38b42
Mitul Panchal <mitul327@gmail.com>
2023-10-02 15:28:38 +0530

    Upgrade Angular & Add Loggers
    - Upgrade to Angular v16
    - Add NgRx Store Devtools and Router Store to App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreRouterConnectingModule } from "ngrx/router-store";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
-import * as fromApp from './store/app.reducer';
-import { AuthEffects } from './auth/store/auth.effects';
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
+import * as fromApp from "./store/app.reducer";
+import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -19,11 +21,13 @@ import { AuthEffects } from './auth/store/auth.effects';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule,
-    StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([AuthEffects])
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
